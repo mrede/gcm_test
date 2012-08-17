0913f8d0683956361f61578a0068ffc5f42b5046
Ben Ede <ben.ede@gmail.com>
2012-08-17 12:11:02 +0100

    blearrghh

--- gcm_daemon.rb
+++ gcm_daemon.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
-Daemons.run_proc('cloud_message_server.rb', {:dir_mode => :normal, :dir => "/opt/pid/sinatra}) do
+Daemons.run_proc('cloud_message_server.rb', {:dir_mode => :normal, :dir => "/opt/pid/sinatra"}) do
 Dir.chdir(pwd)
 exec "ruby cloud_message_server.rb"
 end
